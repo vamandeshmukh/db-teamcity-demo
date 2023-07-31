af2cad06aed6909bcf27068f420345c774234336
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-31 14:31:46 +0530

    updated pom.xml

--- src/main/java/com/db/demo/App.java
+++ src/main/java/com/db/demo/App.java
@@ -6,6 +6,8 @@ package com.db.demo;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Start");
+
+		System.out.println("End");
 	}
 }
