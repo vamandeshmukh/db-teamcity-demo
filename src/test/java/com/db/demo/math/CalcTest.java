acb33161b83dc6e28a0c17a5de4467bb35313c9c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-31 15:24:10 +0530

    updated pom.xml

--- src/test/java/com/db/demo/math/CalcTest.java
+++ src/test/java/com/db/demo/math/CalcTest.java
@@ -1,6 +1,7 @@
 package com.db.demo.math;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +9,16 @@ public class CalcTest {
 
 	Calc calc = new Calc();
 
+	// positive test case 
 	@Test
 	public void testAddNums() {
 		assertEquals(15, calc.addNums(10, 5));
 	}
 
+	// negative test case 
 	@Test
 	public void testAddNums2() {
-		assertEquals(20, calc.addNums(10, 10));
+		assertNotEquals(20, calc.addNums(10, 5));
 	}
+
 }
