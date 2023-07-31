a79000dcbe1f9c9ddccd7ef856dd6fda63d9efa0
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-07-31 16:15:13 +0530

    updated pom.xml

--- src/test/java/com/db/demo/math/CalcTest.java
+++ src/test/java/com/db/demo/math/CalcTest.java
@@ -9,15 +9,21 @@ public class CalcTest {
 
 	Calc calc = new Calc();
 
-	// positive test case 
+	// positive test case
 	@Test
 	public void testAddNums() {
 		assertEquals(15, calc.addNums(10, 5));
 	}
 
-	// negative test case 
+	// another positive test case
 	@Test
 	public void testAddNums2() {
+		assertEquals(20, calc.addNums(10, 10));
+	}
+
+	// negative test case
+	@Test
+	public void testAddNums3() {
 		assertNotEquals(20, calc.addNums(10, 5));
 	}
 
